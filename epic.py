f29c8f6b745fe5641fea1eb69891c9e783b2ee66
gizewashere <gizewashere@outlook.com>
2025-10-25 04:46:00 -0400

    Update epic.py

--- epic.py
+++ epic.py
@@ -51,16 +51,19 @@ with SB(uc=True, test=True,locale=f"{language_code.upper()}") as yopyeaseyoi:
     if is_stream_online("gizewashere"):
         url = "https://www.twitch.tv/gizewashere"
         yopyeaseyoi.uc_open_with_reconnect(url, 5)
-        if yopyeaseyoi.is_element_present('button:contains("Accept")'):
-            yopyeaseyoi.uc_click('button:contains("Accept")', reconnect_time=4)
-        if True:
-            yopyeaseyoi2 = yopyeaseyoi.get_new_driver(undetectable=True)
-            yopyeaseyoi2.uc_open_with_reconnect(url, 5)
-            yopyeaseyoi.sleep(10)
-            if yopyeaseyoi2.is_element_present('button:contains("Accept")'):
-                yopyeaseyoi2.uc_click('button:contains("Accept")', reconnect_time=4)
-            while is_stream_online("gizewashere"):
+        try:
+            if yopyeaseyoi.is_element_present('button:contains("Accept")'):
+                yopyeaseyoi.uc_click('button:contains("Accept")', reconnect_time=4)
+            if True:
+                yopyeaseyoi2 = yopyeaseyoi.get_new_driver(undetectable=True)
+                yopyeaseyoi2.uc_open_with_reconnect(url, 5)
                 yopyeaseyoi.sleep(10)
+                if yopyeaseyoi2.is_element_present('button:contains("Accept")'):
+                    yopyeaseyoi2.uc_click('button:contains("Accept")', reconnect_time=4)
+        except:
+            print("error happened")
+            while is_stream_online("gizewashere"):
+                yopyeaseyoi.sleep(100)
             yopyeaseyoi.quit_extra_driver()
     yopyeaseyoi.sleep(1)
 
